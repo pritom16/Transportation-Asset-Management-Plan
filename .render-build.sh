75a54ef18e1a19bb92c498b75deeb3cd32cbd08a
pritom16 <nixon43@student.sust.edu>
2026-02-19 03:24:13 +0600

    Improve Render build script with system dependencies for geospatial packages

--- .render-build.sh
+++ .render-build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+set -o errexit
+
+# Install system dependencies for geospatial packages
+apt-get update
+apt-get install -y gdal-bin libgdal-dev libgeos-dev libproj-dev
+
+# Upgrade pip and install build tools
+pip install --upgrade pip setuptools wheel
+
+# Install Python dependencies
 pip install -r requirements.txt
